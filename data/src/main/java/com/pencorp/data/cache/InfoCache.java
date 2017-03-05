ee60072b0345d9bf088779bb2c312971f9cd066a
Pentico <morulaneat@gmail.com>
2017-03-05 16:59:41 +0200

    InfoCache setUp

--- data/src/main/java/com/pencorp/data/cache/InfoCache.java
+++ data/src/main/java/com/pencorp/data/cache/InfoCache.java
@@ -4,5 +4,42 @@ package com.pencorp.data.cache;
  * Created by Tuane on 5/03/17.
  */
 
-public class InfoCache {
+import rx.Observable;
+
+/**
+ * An interface representing a info Cache.
+ */
+public interface InfoCache {
+
+    /**
+     * Gets an {@link rx.Observable} which will emit a {@link InfoEntity}.
+     *
+     */
+    Observable<InfoEntity> get();
+
+    /**
+     * Puts and element into the cache.
+     *
+     * @param InfoEntity Element to insert in the cache.
+     */
+    void put(InfoEntity infoEntity);
+
+    /**
+     * Checks if an element exists in the cache.
+     *
+     * @return true if the element is cached, otherwise false.
+     */
+    boolean isCached();
+
+    /**
+     * Checks if the cache is expired.
+     *
+     * @return true, the cache is expired, otherwise false.
+     */
+    boolean isExpired();
+
+    /**
+     * Evict all elements of the cache.
+     */
+    void evictAll();
 }
