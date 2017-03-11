7cc235a4b4cedd0ca61a36e74b81212fa8f781c5
Pentico <morulaneat@gmail.com>
2017-03-11 18:16:02 +0200

    EditActivity and EditFragment

---
+++ Presentation/src/main/java/com/pencorp/cleanarchitecture/view/activity/EditActivity.java
@@ -0,0 +1,17 @@
+package com.pencorp.cleanarchitecture.view.activity;
+
+import com.pencorp.cleanarchitecture.internal.di.HasComponent;
+import com.pencorp.cleanarchitecture.internal.di.components.InfoComponent;
+
+/**
+ * Created by Tuane on 11/03/17.
+ */
+
+public class EditActivity extends BaseActivity implements HasComponent<InfoComponent> {
+
+
+    @Override
+    public InfoComponent getComponent() {
+        return null;
+    }
+}
---
+++ Presentation/src/main/java/com/pencorp/cleanarchitecture/view/fragment/EditFragment.java
@@ -0,0 +1,49 @@
+package com.pencorp.cleanarchitecture.view.fragment;
+
+import android.content.Context;
+
+import com.pencorp.cleanarchitecture.model.InfoModel;
+import com.pencorp.cleanarchitecture.view.EditView;
+
+/**
+ * Created by Tuane on 11/03/17.
+ */
+
+public class EditFragment extends BaseFragment implements EditView {
+
+
+    @Override
+    public void editInfo(InfoModel infoModel) {
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showRetry() {
+
+    }
+
+    @Override
+    public void hideRetry() {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public Context context() {
+        return null;
+    }
+}
